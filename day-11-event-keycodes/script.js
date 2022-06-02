c16a82093eb8c62ba389c192bd03c189da6cc6ff
nerijus-bauza <nerijus.bauza@gmail.com>
2022-06-02 16:54:23 +0300

    Add my solution of Day 11

--- day-11-event-keycodes/script.js
+++ day-11-event-keycodes/script.js
@@ -1,20 +1,61 @@
-const start = document.querySelector('.start');
-const display = document.querySelectorAll('.display');
-
-const eventKey = document.getElementById('eventKey');
-const eventKeyCode = document.getElementById('eventKeyCode');
-const eventCode = document.getElementById('eventCode');
+const container = document.querySelector('.container');
+const keyHistory = []; // array for saving pressed keys info history
 
 document.addEventListener('keydown', (event) => {
+  // prevent default actions for system keys like TAB or ALT
   event.preventDefault();
-  start.classList.add('hide');
-  start.classList.remove('show');
-  display.forEach(el => {
-    el.classList.remove('hide');
-    el.classList.add('show');
+  
+  // save key info into the history array but only if it's not the same as the last pressed key
+  if (!keyHistory.length || event.code !== keyHistory[keyHistory.length - 1].code) {
+    keyHistory.push({
+      key: event.key,
+      keyCode: event.keyCode,
+      code: event.code
+    });
+  }
+    
+  // limit array size to 10 elements
+  if (keyHistory.length > 10) { keyHistory.shift(); }
+
+  // generate html for key info boxes
+  let generatedHtml = `
+    <div class="infobox">${event.key === ' ' ? 'Space' : event.key}<small class="infobox__caption">event.key</small></div>
+    <div class="infobox">${event.keyCode}<small class="infobox__caption">event.keyCode</small></div>
+    <div class="infobox">${event.code}<small class="infobox__caption">event.code</small></div>
+  `;
+  // add history info box
+  generatedHtml = generatedHtml + generateHistory();
+  
+  // finally output generated info to the container
+  container.innerHTML = generatedHtml;
+});
+
+function generateHistory() {
+  if (keyHistory.length < 2) { return ''; } // show history only if there is at least two elements in array
+  
+  // generate captions
+  let content = `
+  <div class="historybox">
+    <div class="historybox__row">
+      <div class="historybox__col historybox__caption">event.key</div>
+      <div class="historybox__col historybox__caption">event.keyCode</div>
+      <div class="historybox__col historybox__caption">event.code</div>
+    </div>`;
+  
+  // loop through array and generate new lines with info
+  keyHistory.forEach(el => {
+    content = content + `
+      <div class="historybox__row">
+        <div class="historybox__col">${el.key}</div>
+        <div class="historybox__col">${el.keyCode}</div>
+        <div class="historybox__col">${el.code}</div>
+      </div>
+    `;
   });
 
-  eventKey.innerHTML = event.key;
-  eventKeyCode.innerHTML = event.keyCode;
-  eventCode.innerHTML = event.code;
-});
+  // close historybox div
+  content = content + `</div>`;
+
+  return content;
+   
+}
