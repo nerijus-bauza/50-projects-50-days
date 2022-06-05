39844c646fa2ea8a6e6285b8126da4eaf803597b
nerijus-bauza <nerijus.bauza@gmail.com>
2022-06-05 13:00:59 +0300

    Add a few tweaks to day 14 after video lesson

--- day-14-animated-navigation/script.js
+++ day-14-animated-navigation/script.js
@@ -3,7 +3,10 @@ const toggle = document.getElementById('toggle');
 const links = document.querySelectorAll('.nav__link');
 
 toggle.addEventListener('click', () => {
+  // toggle class expanded on click
   nav.classList.toggle('expanded');
+
+  // make nav links tabable when in expanded mode
   if (nav.classList.contains('expanded')) {
     links.forEach(link => link.tabIndex = 0);
   } else {
