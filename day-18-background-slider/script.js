41b3973985471872d5aa0db8b78e32fd1a6340b4
nerijus-bauza <nerijus.bauza@gmail.com>
2022-06-22 14:16:47 +0300

    Add day 18 solution

---
+++ day-18-background-slider/script.js
@@ -0,0 +1,33 @@
+const bg = document.querySelector('.bg-box');
+const slides = document.querySelectorAll('.slide');
+
+const btnLeft = document.querySelector('.btn--left');
+const btnRight = document.querySelector('.btn--right');
+
+let activeSlideIndex = 0;
+showSlide(0);
+
+btnLeft.addEventListener('click', () => {
+  activeSlideIndex = (activeSlideIndex - 1) < 0 
+    ? slides.length - 1
+    : activeSlideIndex - 1;
+  console.log(activeSlideIndex);
+  showSlide(activeSlideIndex);
+});
+btnRight.addEventListener('click', () => {
+  activeSlideIndex = (activeSlideIndex + 1) > (slides.length - 1)
+    ? 0
+    : activeSlideIndex + 1;
+    console.log(activeSlideIndex);
+  showSlide(activeSlideIndex);
+});
+
+function showSlide(index) { 
+  slides.forEach(slide => {
+    slide.classList.remove('active');
+  });
+  slides[index].classList.add('active');
+
+  const imgUrl = slides[index].style.getPropertyValue('background-image');
+  bg.style.setProperty('background-image', 'linear-gradient(#0006, #0006), ' + imgUrl); 
+}
